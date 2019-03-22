626343b3fdf77c4987333d77d7b4125f157c15fa
Ian Lessing <ilessing@library.ucsb.edu>
2019-03-22 11:50:29 -0700

    now tracking with git inside RStudio

--- creating_cats_with_yuan.R
+++ creating_cats_with_yuan.R
@@ -1,2 +1,12 @@
 cats <- data.frame(coat=c("calico","black","tabby"),weight=c(2.1,5.0,3.2),likes_string=c(1,0,1))
 write.csv(cats,file="feline.csv",row.names=FALSE)
+cats <- read.csv(file="feline.csv")
+cats
+cats$weight
+
+cats$weight+cats$coat
+cats2<-read.csv(file="~/Desktop/data-shell/feline2.csv")
+
+cats$likes_string<-as.logical(cats$likes_string)
+cats$likes_string
+#re_quize_vector<-c(re_quize_vector,5)
